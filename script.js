691b001d3bc5f6510cab27fbf9ee0df8c07781e4
yiyun12o9 <yoyolee7012@gmail.com>
2022-02-13 16:29:31 +0800

    Mobile Menu

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+	var menu=document.getElementById("menu");
+	menu.classList.toggle("hide");	
+	/*
+	if(menu.style.display=="none"){
+		menu.style.display="block";
+	}else{
+		menu.style.display="none";
+	}
+	*/
+}
